c487f57441e562e6ea038fc9028370b324c5dbd6
Cesar Barraza <cbarrazaa1@gmail.com>
2019-05-10 13:41:58 -0500

    reorganized folder structure

--- src/Engine/Core/Main.h
+++ src/Engine/Core/Main.h
@@ -2,17 +2,17 @@
 #define MAIN_HPP
 
 #ifdef __APPLE__
-	#include "MacOS/MacHelper.hpp"
+	#include "CrossPlatform/MacOS/MacHelper.hpp"
 	MacHelper __macHelper;
 #endif // __APPLE__
 
 #ifdef __linux__
-	#include "Linux/LinuxHelper.hpp"
+	#include "CrossPlatform/Linux/LinuxHelper.hpp"
 	LinuxHelper __linuxHelper;
 #endif // __linux__
 
 #ifdef _WIN32
-	#include "Win32/WindowsHelper.hpp"
+	#include "CrossPlatform/Win32/WindowsHelper.hpp"
 	WindowsHelper __windowsHelper;
 #endif // _WIN32
 
